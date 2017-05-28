a2660ab3d07b961537d079c707d00ae36429ff32
Joachim Holwech <joachim.holwech@gmail.com>
2017-05-28 22:32:29 +0200

    Added a simple websockets example

--- webSocketsTest.go
+++ webSocketsTest.go
@@ -1,51 +1,52 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"github.com/gorilla/websocket"
-	"github.com/holwech/learnxyz-backend/models"
-	_ "github.com/lib/pq"
-	"html/template"
-	"io/ioutil"
-	"log"
 	"net/http"
+	"time"
 )
 
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
 }
 
+type Counter struct {
+	Count int `count:"int"`
+}
+
+// Sends a incrementing count to the client
 func counterHandler(w http.ResponseWriter, r *http.Request) {
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+	counter := Counter{1}
 	fmt.Println("Client subscribed")
-	count := 1
 	for {
 		time.Sleep(2 * time.Second)
-		err = conn.WriteMessage(websocket.TextMessage, count)
+		fmt.Println("Sending ", counter)
+		err = conn.WriteJSON(counter)
+		fmt.Println("Message sent!")
 		if err != nil {
 			fmt.Println(err)
 			break
 		}
-		count++
+		counter.Count++
 	}
 	fmt.Println("Client unsubscribed!")
 }
 
 func main() {
-	index, err := ioutil.ReadAll(indexFile)
-	if err != nil {
-		fmt.Println(err)
-	}
 	http.HandleFunc("/ws", counterHandler)
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello! :)")
 	})
-	fmt.Println("Serving at http://localhost:3000/topics/")
+	fmt.Println("Serving websocket at ws://localhost:3000/ws/")
 	http.ListenAndServe(":3000", nil)
 }
