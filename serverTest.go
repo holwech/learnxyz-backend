d2f7bf97af94062a510521f4b030a56936b93655
Joachim Holwech <joachim.holwech@gmail.com>
2017-06-04 19:36:29 +0200

    Now possible to add new topics directly from the web page

--- serverTest.go
+++ serverTest.go
@@ -9,6 +9,9 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
+	"strings"
+	"time"
 )
 
 type Page struct {
@@ -152,11 +155,82 @@ func addUserHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Please send a request body", 400)
 		return
 	}
-
 }
 
-func topicsHandler(w http.ResponseWriter, r *http.Request) {
-	rows, err := models.Db.Query("SELECT * FROM topics")
+type Status struct {
+	Response    string
+	Description string
+}
+
+const DELAY_ON = true
+
+func simulateDelay(delay int) {
+	if DELAY_ON {
+		time.Sleep(time.Duration(delay) * time.Second)
+	}
+}
+
+// TODO: Ensure unique entries based on topic
+// TODO: Fix case sensitivity
+// TODO: Ensure to filter out unserious entries/weird characters etc
+func addTopic(w http.ResponseWriter, r *http.Request) {
+	simulateDelay(2)
+	fmt.Println("URL accessed: ", r.URL)
+	fmt.Println("Queries are: ", r.URL.Query())
+	queries := r.URL.Query()
+	stmt, err := models.Db.Prepare(`
+		INSERT INTO topics (
+			topic, discipline, sub_discipline, field, description, install_date
+		) VALUES (
+			$1, $2, $3, $4, $5, $6
+		)
+	`,
+	)
+	if err != nil {
+		log.Panic(err)
+	}
+	_, err = stmt.Exec(
+		strings.Join(queries["topic"], ""),
+		strings.Join(queries["discipline"], ""),
+		strings.Join(queries["subDiscipline"], ""),
+		strings.Join(queries["field"], ""),
+		strings.Join(queries["description"], ""),
+		time.Now(),
+	)
+	checkErr(err)
+	status := Status{"Success", "Topic inserted"}
+	if err != nil {
+		status = Status{"Failed", "Insertion failed"}
+	}
+	responseUJson, _ := json.Marshal(status)
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Write(responseUJson)
+}
+
+// TODO: Fix case sensitivity
+func searchTopics(w http.ResponseWriter, r *http.Request) {
+	simulateDelay(2)
+	fmt.Println("URL accessed: ", r.URL)
+	fmt.Println("Queries are: ", r.URL.Query())
+
+	queries := r.URL.Query()
+
+	// Sets the search key
+	sSearch := queries["search"]
+	search := strings.Join(sSearch, "")
+	// Number of results returned
+	limit := 20
+	sLimit, ok := queries["limit"]
+	if ok {
+		limit, err := strconv.ParseInt(strings.Join(sLimit, ""), 10, 0)
+		checkErr(err)
+		if limit > 100 {
+			limit = 100
+		}
+	}
+
+	rows, err := models.Db.Query("SELECT * FROM topics WHERE topic LIKE '%' || $1 || '%' LIMIT $2", search, limit)
 	checkErr(err)
 	topics := []models.Topic{}
 	for rows.Next() {
@@ -185,7 +259,8 @@ func main() {
 	//models.PrintTopics()
 	//HTTP router example
 	http.HandleFunc("/view/", viewHandler)
-	http.HandleFunc("/topics/", topicsHandler)
+	http.HandleFunc("/topics/search", searchTopics)
+	http.HandleFunc("/topics/add", addTopic)
 	//http.HandleFunc("/edit/", editHandler)
 	//http.HandleFunc("/save/", saveHandler)
 	//http.HandleFunc("/changeBody/", changeBodyHandler)
@@ -193,7 +268,10 @@ func main() {
 	//http.HandleFunc("/fruits/", fruitsHandler)
 	//http.HandleFunc("/getUser/", getUserHandler)
 	//http.HandleFunc("/gettopic/", getTopicHandler)
-	fmt.Println("Serving at http://localhost:8091/topics/")
+	if DELAY_ON {
+		fmt.Println("Simulated latency is turned ON")
+	}
+	fmt.Println("Serving at http://localhost:8091/ + /search, /add")
 	http.ListenAndServe(":8091", nil)
 }
 
